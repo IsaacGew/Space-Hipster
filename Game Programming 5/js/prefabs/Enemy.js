3a05b60a74e3cc0fd89d81596e4ecdb2ab167c41
IsaacGew <75827521+IsaacGew@users.noreply.github.com>
2021-06-19 16:14:31 +0800

    Add files via upload

---
+++ Game Programming 5/js/prefabs/Enemy.js
@@ -0,0 +1,71 @@
+var SpaceHipster = SpaceHipster || {}
+
+SpaceHipster.Enemy = function(game, x, y, key, health, enemyBullets) {
+    Phaser.Sprite.call(this, game, x, y, key);
+
+    this.game = game;
+
+    //this.game.physics.arcade.enable(this);
+
+    this.animations.add('getHit', [0, 1, 2, 1, 0], 25, false);
+    this.anchor.setTo(0.5);
+    this.health = health;
+
+    this.enemyBullets = enemyBullets;
+
+    this.enemyTimer = this.game.time.create(false);
+    this.enemyTimer.start()
+
+    this.scheduleShooting();
+
+};
+
+SpaceHipster.Enemy.prototype = Object.create(Phaser.Sprite.prototype);
+SpaceHipster.Enemy.prototype.constructor = SpaceHipster.Enemy;
+
+SpaceHipster.Enemy.prototype.update = function() {
+    if(this.x < 0.05 * this.game.world.width) {
+        this.x = 0.05 * this.game.world.width + 2;
+        this.body.velocity.x *= -1;
+    }
+    else if(this.x > 0.95 * this.game.world.width) {
+        this.x = 0.95 * this.game.world.width - 2;
+        this.body.velocity.x *= -1;
+    }
+    if(this.top > this.game.world.height) {
+        this.kill();
+    }
+};
+
+SpaceHipster.Enemy.prototype.damage = function(amount) {
+    Phaser.Sprite.prototype.damage.call(this, amount);
+    this.play('getHit')
+
+    if(this.health <= 0) {
+        var emitter = this.game.add.emitter(this.x, this.y, 250);
+        emitter.makeParticles('enemyParticle');
+        emitter.minParticleSpeed.setTo(-200, -200);
+        emitter.maxParticleSpeed.setTo(200, 200);
+        emitter.gravity = 0;
+        emitter.start(true, 1000, null, 100);
+    }
+};
+
+SpaceHipster.Enemy.prototype.scheduleShooting = function() {
+    this.shoot();
+
+    this.enemyTimer.add(Phaser.Timer.SECOND * 1, this.scheduleShooting, this);
+};
+
+SpaceHipster.Enemy.prototype.shoot = function() {
+    var bullet = this.enemyBullets.getFirstExists(false);
+
+    if(!bullet) {
+        bullet = new SpaceHipster.enemyBullet(this.game, this.x, this.bottom);
+        this.enemyBullet.add(bullet);
+    } else {
+        bullet.reset(tis.x, this.y);
+    }
+
+    bullet.body.velocity.y = 100;
+}
